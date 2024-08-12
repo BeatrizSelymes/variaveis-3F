ba97e94afa7d366362bd7cc66fb7c25258c7d0bf
BeatrizSelymes <164294239+BeatrizSelymes@users.noreply.github.com>
2024-08-12 23:10:39 +0000

    12/08/24-2

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const idade = 17;
+let idade = 17;
 const anoNascimento = 2007;
 let anoAtual = 2024;
+const nomeCompleto = "Beatriz Selymes"
+
 
 anoAtual = anoAtual + 1;
+idade = idade + 1;
 
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual "  + anoAtual + " completo" + idade + " anos");
 console.log(anoAtual);
+console.log(idade);
+
