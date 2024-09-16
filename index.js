ec014f9dd2bc4b5116cf2bf6cbbc0b8960996eeb
BeatrizSelymes <164294239+BeatrizSelymes@users.noreply.github.com>
2024-09-16 23:06:38 +0000

    16/09/24

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
+const prompt = require('prompt-sync');
+const entrada = prompt ();
+
+
 let idade = 17;
 const anoNascimento = 2007;
 let anoAtual = 2024;
-const nomeCompleto = "Beatriz Selymes"
+const nomeCompleto = "Beatriz Santos Selymes";
 
+console.log("Meu nome é " + nomeCompleto + "Nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos")
 
 anoAtual = anoAtual + 1;
-idade = idade + 1;
 
-console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual "  + anoAtual + " completo " + idade + " anos");
 console.log(anoAtual);
+
+idade = idade + 1;
 console.log(idade);
 
-const listaDeDestinos = Array(
-  " Buenos Aires",
-  " Canada",
-  " Tokyo",
-  " Nova York",
-  " Luxemburgo"
+
+//Agência de viagens Los Angeles, Nova York, Alemanha, Londres, Tokio
+const listaDeDestinos = new Array(
+  " Grécia",    
+  " Cacun",
+  " Luxemburgo",
+  " Hungria",
+  " Tokyo"
 );
 
-const idadeComprador = 18;
+var nomeComprador = entrada ("Digite seu nome:");
+var idadeComprador = entrada ("Qual a sua idade?");
 
-if(idadeComprador >= 18){
+
+if(idadeComprador  >=18) {
+   console.log(" Ola " + nomeComprador);
   console.log("Comprador maior de idade");
-  console.log(" Destinos possiveis: " +  listaDeDestinos);
-} else { 
-  console.log("Comprador menor de idade");
-  console.log("Você precisa ter 18 anos ou mais para comprar passagens. Por favor, peça ajuda a um adulto.");
+  console.log("destinos disponíveis: " + listaDeDestinos);
+} else {
+  console.log(" Ola " + nomeComprador);
+  console.log(" Comprador menor de idade ");
+  console.log(" Infelizmente você não pode comprar a passagem ");
 }
-
-
-
