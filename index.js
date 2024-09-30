53b7ae9e35737e7ab4df6eda6e1e66b38554934d
anawsnts <31930734-anawsnts@users.noreply.replit.com>
2024-09-30 23:03:33 +0000

    30/09/24

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const entrada = prompt ();
 let idade = 17;
 const anoNascimento = 2007;
 let anoAtual = 2024;
-const nomeCompleto = "Beatriz Santos Selymes";
+const nomeCompleto = "Beatriz Selymes";
 
-console.log("Meu nome é " + nomeCompleto + "Nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos")
+console.log("Meu nome é " + nomeCompleto + " Nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos")
 
 anoAtual = anoAtual + 1;
 
@@ -29,27 +29,37 @@ const listaDeDestinos = new Array(
   " Maldivas",
   );
 
-var nomeComprador = entrada ("Digite seu nome:");
-var idadeComprador = entrada ("Qual a sua idade?");
+var nomeComprador = entrada ("Digite seu nome: ");
+var idadeComprador = entrada ("Qual a sua idade? ");
 
+const loginCerto = "Beatriz";
+const senhaCerto = "2007";
+
+var login = entrada ("Digite seu login: ");
+var senha = entrada ("Digite sua senha: ");
+
+while(login != loginCerto && senha != senhaCerto){
+  console.log("Login ou senha incorretos");
+  login = entrada ("Digite seu login: ");
+  senha = entrada ("Digite sua senha: ");
+}
 
 if(idadeComprador  >=18) {
    console.log(" Ola " + nomeComprador);
   console.log("Comprador maior de idade");
-  
+
   var contador = 0;
   while(contador <9 ){
     console.log(listaDeDestinos[contador]);
     contador++;
-  
+
   }
-  var destino = entrada("Digite o número que corresponde ao destino selecinado")
+  var destino = entrada("Digite o número que corresponde ao destino selecinado: ")
   console.log("destino selecionado foi: ${listaDeDestinos[destino]}}");
-  
+
 } else {
   console.log(" Ola " + nomeComprador);
   console.log(" Comprador menor de idade ");
   console.log(" Infelizmente você não pode comprar a passagem ");
 
 }
-
